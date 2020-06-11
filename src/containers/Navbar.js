8da2ed9094834309a16f0eb4a3aac1f94429b90d
katerinafaustenova <katerina.faustenova@seznam.cz>
2020-06-11 15:36:46 +0200

    dropdown

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -2,28 +2,61 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
-function Navbar() {
-  return (
-    <nav className="nav_left">
-      <ul>
-        <li>
-          <NavLink to="/ReactRouter" activeClassName="active">
-            React Router
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Errors" activeClassName="active">
-            Errors
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/CSSTemplates" activeClassName="active">
-            CSS Templates
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: "",
+    };
+  }
+
+  handleClick = (par) => {
+    if (this.state.open !== par) {
+      return this.setState(() => ({ open: par }));
+    }
+    this.setState(() => ({ open: "" }));
+  };
+
+  render() {
+    return (
+      <nav className="nav_left">
+        <ul>
+          <li>
+            <NavLink
+              to="/HTML"
+              activeClassName="active"
+              onClick={() => this.handleClick("HTML")}
+            >
+              HTML
+            </NavLink>
+            {this.state.open === "HTML" && (
+              <div className="dropdown">
+                <NavLink to="/HTML/1" exact>
+                  HTML 1
+                </NavLink>
+                <NavLink to="/HTML/2" exact>
+                  HTML 2
+                </NavLink>
+                <NavLink to="/HTML/3" exact>
+                  HTML 3
+                </NavLink>
+              </div>
+            )}
+          </li>
+          <li>
+            <NavLink to="/CSS" onClick={() => this.handleClick("CSS")}>
+              CSS
+            </NavLink>
+            {this.state.open === "CSS" && (
+              <div className="dropdown">
+                <NavLink to="/CSS/1">CSS 1</NavLink>
+              </div>
+            )}
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
