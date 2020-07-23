1e7a4f64b31b6f7d933e0e0f8b04d56f675e8f7d
katerinafaustenova <katerina.faustenova@seznam.cz>
2020-07-23 16:41:30 +0200

    state and props page update

--- src/pages/StateProps/StatePropsPage.js
+++ src/pages/StateProps/StatePropsPage.js
@@ -38,16 +38,76 @@ const code_box_props = `function Box(props) {
 function StatePropsPage() {
   return (
     <React.Fragment>
-      <h1>State and Props</h1>
-      <p>Description</p>
-      <div className="syntax_highlight_react_state">
-        <SyntaxHighlighter language="javascript" style={xonokai}>
+      <div className="stateprops_page">
+        <h1>State and Props</h1>
+        <h2>Explanation and usage</h2>
+        <p>
+          React state is an object that represents the parts of the app that can
+          change.
+        </p>
+        <p>
+          State is needed when we want our app to do anything (interact like add
+          or delete stuff, log in or log out from a user acount).
+        </p>
+        <p>
+          Each component can maintain its own state in an object called
+          this.state in a key/value pairs.
+        </p>
+        <p>Only class component can use state as an example below !</p>
+        <p>
+          For changing the current state we need to explicitly said that to
+          React, and we do that by calling this.setState method inside a
+          component.
+        </p>
+        <p>
+          When changing the state, React will automatically re-render every time
+          we call setState method.
+        </p>
+        <p>
+          Very important is NOT to change this.state directly. Use
+          this.setState.
+        </p>
+        <p>
+          State changes are asynchronous. If we read from this.state immediately
+          after calling this.setState, it probably won’t reflect the changes
+          yet.
+        </p>
+        <SyntaxHighlighter
+          language="javascript"
+          style={xonokai}
+          className="syntax_highlighter"
+        >
           {code_home_state}
         </SyntaxHighlighter>
-        <SyntaxHighlighter language="javascript" style={xonokai}>
+        <SyntaxHighlighter
+          language="javascript"
+          style={xonokai}
+          className="syntax_highlighter"
+        >
           {code_home_render}
         </SyntaxHighlighter>
-        <SyntaxHighlighter language="javascript" style={xonokai}>
+        <p>
+          React props (properties) allow us to pass some data from one component
+          to another.
+        </p>
+        <p>
+          Props can be passed only one way - from top to bottom, which means
+          from parent to its children.
+        </p>
+        <p>
+          Important information is that props are read-only. So the data coming
+          from a parent should NOT be changed by its children.
+        </p>
+        <p>
+          To pass data, first we need to set attribute and its value. Then we
+          can use props keyword to pass it to its children as shown below with
+          the Box component.
+        </p>
+        <SyntaxHighlighter
+          language="javascript"
+          style={xonokai}
+          className="syntax_highlighter"
+        >
           {code_box_props}
         </SyntaxHighlighter>
       </div>
