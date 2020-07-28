16ed733c492e699ca1f68ae3ca77497ab807f72c
katerinafaustenova <katerina.faustenova@seznam.cz>
2020-07-28 20:57:59 +0200

    calculator styling

--- src/components/TipCalculator/TipCalculator.js
+++ src/components/TipCalculator/TipCalculator.js
@@ -6,27 +6,29 @@ class TipCalculator extends React.Component {
     super();
     this.state = {
       bill: null,
-      selectedOption: 0.2,
+      selectedOption: 10,
       tip: null,
       total: null,
     };
   }
 
-  handleChangeBill = (e) => {
+  // componentDidUpdate() {
+  //   return;
+  // }
+
+  handleChange = (e) => {
+    e.preventDefault();
     this.setState({
       [e.target.id]: Number(e.target.value),
     });
-  };
-
-  handleChangeOption = (e) => {
-    this.setState({ selectedOption: e.target.value });
+    this.handleSubmit(e);
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const { bill, selectedOption } = this.state;
 
-    const result = bill * selectedOption;
+    const result = (bill * selectedOption) / 100;
     const total = result + bill;
 
     this.setState({
@@ -36,37 +38,44 @@ class TipCalculator extends React.Component {
   };
 
   render() {
-    console.log(this.state.bill, "render");
     return (
-      <React.Fragment>
-        <form onSubmit={this.handleSubmit} className="calculator">
-          <label>
-            How much was your bill ?
-            <input
-              type="number"
-              // value={this.state.bill}
-              id="bill"
-              onChange={this.handleChangeBill}
-              min="0"
-            />
-          </label>
-          <label>
-            How was your service ?
-            <select
-              value={this.state.selectedOption}
-              onChange={this.handleChangeOption}
-            >
-              <option value="0.20">20 % Absolutely perfect</option>
-              <option value="0.15">15 % Very good</option>
-              <option value="0.10">10 % Good / Satisfying</option>
-              <option value="0.05">5 % Not that good</option>
-            </select>
-          </label>
-          <input type="submit" value="Calculate" />
-        </form>
-        <div>Tip: {this.state.tip}</div>
-        <div>Total amount: {this.state.total}</div>
-      </React.Fragment>
+      <form className="calculator">
+        <div className="row">
+          <div className="label">Bill</div>
+          <input
+            type="number"
+            // value={this.state.bill}
+            id="bill"
+            onChange={this.handleChange}
+            min="0"
+          />
+        </div>
+        <div className="row">
+          <div className="label">Tip</div>
+          <div>{this.state.selectedOption} %</div>
+        </div>
+        <div className="row">
+          <input
+            type="range"
+            min="0"
+            max="30"
+            id="selectedOption"
+            value={this.state.selectedOption}
+            onChange={this.handleChange}
+            step="5"
+            className="slider"
+          ></input>
+        </div>
+        <div className="line"></div>
+        <div className="row">
+          <div>Tip amount:</div>
+          <div>{this.state.tip}</div>
+        </div>
+        <div className="row">
+          <div>Total amount:</div>
+          <div className="total">{this.state.total}</div>
+        </div>
+      </form>
     );
   }
 }
--- src/containers/Content.js
+++ src/containers/Content.js
@@ -12,7 +12,7 @@ import "./Content.css";
 function Content() {
   return (
     <div className="content_right">
-      <Redirect to="/React/ToDoPage" />
+      <Redirect to="/React/TipCalculatorPage" />
       <Switch>
         <Route path="/home">
           <Home />
